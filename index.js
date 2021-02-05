e7f13a950d488b989e119ab9194a3a2d1aec0e6c
Rebecca Howe <Rebeccahowe20@gmail.com>
2021-02-04 21:14:17 -0800

    create connection to mysql database

--- index.js
+++ index.js
@@ -3,3 +3,31 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 
+
+// mysql connection
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Fru1tBa$ket',
+    database: 'employee_trackerDB'
+});
+console.log(connection)
+    connection.connect((err) => {
+        if (err) throw err;
+    });
+
+// inquirer prompt
+// const questions = () => {
+
+//     inquirer.prompt([
+//         {
+
+//         }
+//     ]);
+// }
+
+// create prompts to collect data
+// push data to array
+// push array to mysql database
